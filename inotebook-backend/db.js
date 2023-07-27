f2c07b9b5c7f3999558d4299d0182f3536090b9f
Abhishek Kapoor <kapoorkk1980@gmail.com>
2023-07-27 11:48:29 +0530

    storing data into database using mongoose

--- inotebook-backend/db.js
+++ inotebook-backend/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
 
-const mongoURI = "mongodb://localhost:27017/"
+const mongoURI = "mongodb://127.0.0.1:27017/test?readPreference=primary&tls=false&directConnection=true"
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURI)
-    console.log("connected")
+const connectToMongo = async () => {
+    console.log("Trying to connect")
+    await mongoose.connect(mongoURI)
+    console.log("Connected")
 }
 
 module.exports = connectToMongo;
--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    
-    res.json([])
+// Make user using post request on /api/auth
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    const user = new User(req.body)
+    await user.save()
+    res.json(req.body)
 })
 
 module.exports = router
