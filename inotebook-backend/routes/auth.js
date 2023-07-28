73cb869f6d8b8c699c2580a97fb8133f980d058c
Abhishek Kapoor <kapoorkk1980@gmail.com>
2023-07-28 16:24:36 +0530

    managing user creation

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,29 +1,41 @@
-const express = require('express')
-const User = require('../models/User')
+const express = require('express');
+const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
-// Make user using post request on /api/auth
-router.post('/', [
+// Make user using post request on /api/auth/createuser
+router.post('/createuser/', [
     body('name', 'Enter a valid name').isLength({ min: 3, max: 25 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 8 character long').isLength({ min: 8 })
 ], async (req, res) => {
 
+    // Check for errors and return errors if they exists
     const result = validationResult(req);
     if (result.isEmpty()) {
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }).then(user => res.json(user))
-        .catch(error => {
-            res.json({error: "Please enter a unique value of email!", message: error.message })
-        })
-        
+        try {
+            // Check wether a user exists with same email already
+            let user = await User.findOne({ email: req.body.email }).exec();
+            if (user) {
+                return res.status(400).json({ error: "Sorry a user with this email already exists!", user: user })
+            } else {
+                // create user if it doesn't exists
+                user = await User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                return res.json(user)
+            }
+
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({ error: error })
+        }
     } else {
         return res.send({ errors: result.array() });
     }
+
 })
 
 module.exports = router
